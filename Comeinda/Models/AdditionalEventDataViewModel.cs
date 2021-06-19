3bd9e14372743f089211d3eef48b4662bbe802eb
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-06-19 19:40:36 +0300

    add view models

---
+++ Comeinda/Models/AdditionalEventDataViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Comeinda.Data.Tables.Enams;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Comeinda.Models
+{
+    public class AdditionalEventDataViewModel
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public EventStatusEnum Status { get; set; }
+        [Required]
+        public string TitleSEO { get; set; }
+        [Required]
+        public string DescriptionSEO { get; set; }
+        [Required]
+        public string KeywordsSEO { get; set; }
+        [Required]
+        public string LinkURL { get; set; }
+    }
+}
---
+++ Comeinda/Models/Files/EventFilesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Comeinda.Models.Files
+{
+    public class EventFilesViewModel
+    {
+        public Guid EventId { get; set; }
+        public string NameEvent { get; set; }
+        public int Files { get; set; }
+    }
+}
