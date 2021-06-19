6fa349289ea949ae2a04bb23ff22318f8b4d9c2c
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-06-19 19:38:35 +0300

    add tickets controller

---
+++ Comeinda/Controllers/TicketsController.cs
@@ -0,0 +1,22 @@
+﻿using Comeinda.Data.Repositories.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Comeinda.Controllers
+{
+    public class TicketsController : Controller
+    {
+        private readonly ITicketRepository ticketRepository;
+
+        public TicketsController(ITicketRepository ticketRepository)
+        {
+            this.ticketRepository = ticketRepository;
+        }
+
+        // GET: Ticket
+        public async Task<IActionResult> Index()
+        {
+            return View(await ticketRepository.GetAllAsync());
+        }
+    }
+}
